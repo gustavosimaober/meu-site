6cbea7a6e482860f4776d29e863cb6329f39d1b4
gustavosimaober <140729114+gustavosimaober@users.noreply.github.com>
2024-05-21 08:40:10 -0300

    subi os arquivos do meu site

---
+++ pagina2.js
@@ -0,0 +1,14 @@
+var quotes = [
+   
+    'Life is what happens to us while we are making other plans',
+    'Inspirational Quotes From Fairy Tales',
+     
+
+
+]    
+
+
+function newQuote() {
+    var randomNumber = Math.floor(Math.random() * (quotes.length));
+    document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+     }
